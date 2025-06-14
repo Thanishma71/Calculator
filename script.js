de9e70b5d8c759f56c57285e92460467a143bc1e
Thanishma71 <153097210+Thanishma71@users.noreply.github.com>
2025-06-14 21:09:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendValue(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+  try {
+    const result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
+}
+
+function backspace() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
